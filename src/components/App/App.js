dfd5e9ae4118f22408f8036e134589b30e83b4ac
Thomas Roselyn <tgroselyn@gmail.com>
2019-08-14 16:12:47 -0500

    initial setup

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img class="boombox" src="images/philips-boombox.jpg" alt="philips boombox"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
